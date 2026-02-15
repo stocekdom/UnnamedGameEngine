d21c45a6dc0c50b2f2e0900aaaba303def9f84f7
stocekdom <dominikstocek2@gmail.com>
2026-02-15 16:37:06 +0100

    Improve JSON type + Add line and column to Token + Add a custom JSON exception

--- Engine/JSON/JSONTypes.h
+++ Engine/JSON/JSONTypes.h
@@ -5,8 +5,10 @@
 #ifndef GAME1_JSONTYPES_H
 #define GAME1_JSONTYPES_H
 #include <optional>
+#include <stdexcept>
 #include <string>
 #include <unordered_map>
+#include <utility>
 #include <variant>
 #include <vector>
 
@@ -18,22 +20,39 @@ using JsonNull = std::nullopt_t;
 
 struct Json
 {
-   Json( Json&& other ) = default;
+   using Value = std::variant<double, JsonNull, bool, JsonObject, JsonArray, std::string>;
 
-   Json& operator=( Json&& other ) = default;
+   Json() : value( std::nullopt )
+   {
+   }
 
-   using Value = std::variant<JsonNull, bool, double, JsonObject, JsonArray, std::string>;
+   explicit Json( Value&& v ) : value( std::move( v ) )
+   {
+   }
+
+   explicit Json( const Value& v ) : value( v )
+   {
+   }
+
+   Json( const Json& other ) = default;
+
+   Json( Json&& other ) noexcept = default;
+
+   Json& operator=( const Json& other ) = default;
+
+   Json& operator=( Json&& other ) noexcept = default;
 
    Value value;
 };
 
-enum class TokenType {
-   LBrace,     // {
-   RBrace,     // }
-   LBracket,   // [
-   RBracket,   // ]
-   Colon,      // :
-   Comma,      // ,
+enum class TokenType
+{
+   LBrace, // {
+   RBrace, // }
+   LBracket, // [
+   RBracket, // ]
+   Colon, // :
+   Comma, // ,
 
    String,
    Number,
@@ -49,5 +68,16 @@ struct Token
 {
    std::string_view text;
    TokenType type;
+   size_t line = 0;
+   size_t column = 0;
 };
+
+class JSONException : public std::runtime_error
+{
+   public:
+      explicit JSONException( const std::string& message ) : std::runtime_error( message )
+      {
+      }
+};
+
 #endif //GAME1_JSONTYPES_H
