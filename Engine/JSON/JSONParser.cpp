090a1b217d893ced1b2f2c90a4b48fa8d50d0f3b
stocekdom <dominikstocek2@gmail.com>
2026-02-15 18:22:22 +0100

    Implement JSON Parser

--- Engine/JSON/JSONParser.cpp
+++ Engine/JSON/JSONParser.cpp
@@ -3,16 +3,168 @@
 //
 
 #include "JSONParser.h"
+#include "../Logging/Logger.h"
+#include <charconv>
+#include <fstream>
 
-JsonObject JSONParser::parseFromFile( const std::string& path )
+Json JSONParser::parseFromFile( const std::string& path )
 {
+   std::ifstream file( path, std::ios::binary );
 
+   if( !file )
+      LOG_ERROR( "Couldn't open JSON file " + path );
+
+   file.seekg( 0, std::ios::end );
+   auto size = file.tellg();
+   file.seekg( 0 );
+
+   // TODO implement streaming solution for large files
+   if( size > MAX_JSON_IN_MEMORY_SIZE )
+      throw JSONException( "JSON file is too large to process" );
+
+   std::string buffer( size, '\0' );
+   file.read( buffer.data(), size );
+
+   return parseFromString( buffer );
 }
 
-JsonObject JSONParser::parseFromString( const std::string& jsonString )
+Json JSONParser::parseFromString( const std::string& jsonString )
 {
+   tokenizer = std::make_unique<JSONTokenizer>( jsonString );
+   // Initialize the token to the first token of the string
+   currentToken = tokenizer->getNextToken();
+
+   return parseValue();
 }
 
-JsonObject JSONParser::parseImpl( const std::string& data )
+Token JSONParser::matchToken( const TokenType type )
 {
+   if( currentToken.type != type )
+      throw JSONException(
+         "Invalid token " + std::string( currentToken.text ) + " at line " + std::to_string( currentToken.line ) + " position " +
+         std::to_string( currentToken.column ) );
+
+   auto prev = currentToken;
+   currentToken = tokenizer->getNextToken();
+   return prev;
+}
+
+void JSONParser::expectToken( TokenType type )
+{
+   // Silences the no_discard warning
+   ( void )matchToken( type );
+}
+
+Json JSONParser::parseNumber()
+{
+   auto prevToken = matchToken( TokenType::Number );
+
+   double value{};
+   const auto& s = prevToken.text;
+   auto [ ptr, ec ] = std::from_chars( s.data(), s.data() + s.size(), value );
+
+   if( ec == std::errc() && ptr == s.data() + s.size() )
+      return Json{ value };
+
+   throw JSONException(
+      "Invalid number at line " + std::to_string( prevToken.line ) + " position " + std::to_string( prevToken.column ) );
+}
+
+Json JSONParser::parseString()
+{
+   auto prevToken = matchToken( TokenType::String );
+   return Json{ std::string( prevToken.text ) };
+}
+
+Json JSONParser::parseObject()
+{
+   expectToken( TokenType::LBrace );
+   JsonObject object;
+
+   // Handle empty object {}
+   if( currentToken.type == TokenType::RBrace )
+   {
+      expectToken( TokenType::RBrace );
+      return Json{ std::move( object ) };
+   }
+
+   while( true )
+   {
+      auto key = matchToken( TokenType::String );
+      expectToken( TokenType::Colon );
+      auto val = parseValue();
+      auto [ it, inserted ] = object.try_emplace( std::string( key.text ), std::move( val ) );
+
+      if( !inserted )
+         throw JSONException(
+            "Duplicate key " + std::string( key.text ) + " at line " + std::to_string( key.line ) + " position " +
+            std::to_string( key.column ) );
+
+      if( currentToken.type == TokenType::RBrace )
+         break;
+
+      expectToken( TokenType::Comma );
+   }
+
+   expectToken( TokenType::RBrace );
+   return Json{ std::move( object ) };
+}
+
+Json JSONParser::parseArray()
+{
+   expectToken( TokenType::LBracket );
+   JsonArray array;
+
+   // Handle empty array []
+   if( currentToken.type == TokenType::RBracket )
+   {
+      expectToken( TokenType::RBracket );
+      return Json{ std::move( array ) };
+   }
+
+   while( true )
+   {
+      array.emplace_back( parseValue() );
+
+      if( currentToken.type == TokenType::RBracket )
+         break;
+
+      expectToken( TokenType::Comma );
+   }
+
+   expectToken( TokenType::RBracket );
+   return Json{ std::move( array ) };
+}
+
+Json JSONParser::parseValue()
+{
+   switch( currentToken.type )
+   {
+      case TokenType::False:
+      {
+         auto val = currentToken.type == TokenType::True;
+         expectToken( TokenType::False );
+         return Json{ val };
+      }
+      case TokenType::True:
+      {
+         auto val = currentToken.type == TokenType::True;
+         expectToken( TokenType::True );
+         return Json{ val };
+      }
+      case TokenType::Null:
+         return Json{ std::nullopt };
+      case TokenType::Number:
+         return parseNumber();
+      case TokenType::String:
+         return parseString();
+      case TokenType::LBracket:
+         return parseArray();
+      case TokenType::LBrace:
+         return parseObject();
+      default:
+         throw JSONException(
+            "Invalid value at line " + std::to_string( currentToken.line ) + " position " +
+            std::to_string( currentToken.column ) );
+   }
 }
--- Engine/JSON/JSONParser.h
+++ Engine/JSON/JSONParser.h
@@ -5,17 +5,53 @@
 #ifndef GAME1_JSONPARSER_H
 #define GAME1_JSONPARSER_H
 
+#include <memory>
+
+#include "JSONTokenizer.h"
 #include "JSONTypes.h"
 
 class JSONParser
 {
    public:
-      static JsonObject parseFromFile( const std::string& path );
+      Json parseFromFile( const std::string& path );
 
-      static JsonObject parseFromString( const std::string& jsonString );
+      Json parseFromString( const std::string& jsonString );
 
    private:
-      static JsonObject parseImpl( const std::string& data );
+      // This limits the size of the JSON we want to fit into a string. If the JSON is larger, we need to parse it using streams
+      static constexpr long MAX_JSON_IN_MEMORY_SIZE = 16777216;
+      Token currentToken = { {}, TokenType::Error };
+      // Unique ptr since tokenizer is one time use only for one input
+      std::unique_ptr<JSONTokenizer> tokenizer;
+
+      /**
+       * Tries to match the current token with a given type
+       * If the tokens match, it loads another token into currentToken and returns the previous currentToken.
+       * If the types don't match, an exception is thrown
+       * @param type The token type to match
+       * @return The matched token
+       * @throws JSONException
+       */
+      [[nodiscard]] Token matchToken( TokenType type );
+
+      /**
+       * Tries to match the current token with a given type
+       * If the tokens match, it loads another token into currentToken, but doesnt return anything, unlike matchToken
+       * @see matchToken
+       * If the types don't match, an exception is thrown
+       * @param type
+       */
+      void expectToken( TokenType type );
+
+      [[nodiscard]] Json parseNumber();
+
+      [[nodiscard]] Json parseString();
+
+      [[nodiscard]] Json parseObject();
+
+      [[nodiscard]] Json parseArray();
+
+      Json parseValue();
 };
 
 #endif //GAME1_JSONPARSER_H
